9a8d7e5ac6f2bd720c5d804a839fdf813a2eddee
Lawrence <lawrey7@gmail.com>
2020-03-10 14:36:06 -0700

    Added new styling to Header component.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,16 +4,16 @@ class Header extends React.Component {
   render() {
     return (
       <div className="page-header">
-        <h1 className="ml-5 mb-5 d-flex flex-wrap justify-content-between">
-          <p onClick={() => this.props.setView('catalog', {})}
+        <div className="ml-5 mb-5 d-flex flex-wrap justify-content-between">
+          <h1 onClick={() => this.props.setView('catalog', {})}
             style={{ cursor: 'pointer' }}>
             Wicked Sales
-          </p>
+          </h1>
           <div className="d-flex flex-wrap align-items-center">
-            <i className="fas fa-shopping-cart fa-xs mr-2"></i>
-            <p className="mr-5">{this.props.cartItemCount}</p>
+            <h5 className="mr-2">{this.props.cartItemCount} {this.props.cartItemCount === 1 ? 'item' : 'items'}</h5>
+            <i className="fas fa-shopping-cart fa-2x mr-5"></i>
           </div>
-        </h1>
+        </div>
       </div>
     );
   }
