380dabb31399c5068ff04228470eae4ec8fcb7a3
Lawrence <lawrey7@gmail.com>
2020-03-10 15:23:08 -0700

    Added styles to the CartSummaryItems.

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -4,11 +4,15 @@ class CartSummaryItem extends React.Component {
   render() {
     const product = this.props.product;
     return (
-      <div className="card container">
-        <img src={product.image} className="card-img-top h-50 w-50" />
-        <h5 className="cart-title">{product.name}</h5>
-        <h5 className="text-muted">${(product.price / 100).toFixed(2)}</h5>
-        <h5>{product.desc}</h5>
+      <div className="card mb-5">
+        <div className="row h-75">
+          <img src={product.image} className="col-4" style={{ height: '300px' }} />
+          <div className="col-8">
+            <h5 className="card-title">{product.name}</h5>
+            <h5 className="text-muted">${(product.price / 100).toFixed(2)}</h5>
+            <p>{product.shortDescription}</p>
+          </div>
+        </div>
       </div>
     );
   }
