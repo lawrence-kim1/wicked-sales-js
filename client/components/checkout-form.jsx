985146c6cc0d67d00d06879a6b1de98e89c53cc6
Lawrence <lawrey7@gmail.com>
2020-03-10 17:52:59 -0700

    Added styles to the CheckoutForm component.

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -10,16 +10,41 @@ class CheckoutForm extends React.Component {
   }
 
   render() {
+    let itemTotal = 0;
+    this.props.cart.forEach(product => {
+      itemTotal += product.price;
+    });
+
     return (
       <form className="container">
-        <label htmlFor="name" >Name</label>
-        <input type="text" id="name" placeholder="Name" />
-        <label htmlFor="credit">Credit Card</label>
-        <input type="number" id="credit" placeholder="Credit Card Number" />
-        <label htmlFor="shipping">Shiping Address</label>
-        <textarea id="shipping"></textarea>
+        <h1 className="mb-3">My Cart</h1>
+        <h5 className="text-muted mb-3">
+          Order Total: ${(itemTotal / 100).toFixed(2)}
+        </h5>
+        <div className="d-flex flex-column mb-3 form-group">
+          <label htmlFor="name" >Name</label>
+          <input type="text"
+            id="name"
+            placeholder="Name"
+            className="form-control" />
+        </div>
+        <div className="d-flex flex-column mb-3 form-group">
+          <label htmlFor="credit">Credit Card</label>
+          <input type="number"
+            id="credit"
+            placeholder="Credit Card Number"
+            className="form-control" />
+        </div>
+        <div className="d-flex flex-column mb-3 form-group">
+          <label htmlFor="shipping">Shipping Address</label>
+          <textarea id="shipping" className="form-control" rows="5"></textarea>
+        </div>
         <div className="d-flex justify-content-between">
-          <p className="text-muted">&lt; Continue Shopping</p>
+          <p className="text-muted"
+            onClick={() => this.setState('catalog', {})}
+            style={{ cursor: 'pointer' }}>
+            &lt; Continue Shopping
+          </p>
           <button className="btn btn-primary">Place Order</button>
         </div>
       </form>
