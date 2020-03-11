1078c752bf171d7859d984a0ceb41ddeaafd313d
Lawrence <lawrey7@gmail.com>
2020-03-10 17:30:38 -0700

    Added a CheckoutForm component that currently returns null.

---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: ''
+    };
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default CheckoutForm;
